394f528c79104e575a5fdbd85440ec0fbc4c51c8
Wizard-Wade <wade.vollink@gmail.com>
2025-05-20 22:14:22 -0600

    create popup window

---
+++ Window.py
@@ -0,0 +1,31 @@
+from tkinter import Tk, BOTH, Canvas
+from Line import line
+
+class Window:
+    def __init__(self, width, height):
+        self._tk_root = Tk()
+        self._tk_root.title = "Maze Solver"
+        self._canvas = Canvas(self._tk_root, bg = "white", height=height, width=width)
+        self._canvas.pack()
+        self._run_state = False
+        self._tk_root.protocol("WM_DELETE_WINDOW", self.close)
+        
+    def redraw(self):
+        self._tk_root.update_idletasks()
+        self._tk_root.update()
+        
+    def wait_for_close(self):
+        self._run_state = True
+        while self._run_state:
+            self.redraw()
+        print("Window closed")
+            
+    def close(self):
+        self._run_state = False
+        
+    
+    def draw_line(self, line: line, fill_color: str):
+        line.draw(self._canvas, fill_color)
+        
+
+        
